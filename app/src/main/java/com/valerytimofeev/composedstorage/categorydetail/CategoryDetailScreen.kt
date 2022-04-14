9209c689582d42b72ed30c6e7d3c15478cce9e92
llin first <90470991+Haartag@users.noreply.github.com>
2022-04-14 13:20:35 +0300

    Add change dialog for items

--- app/src/main/java/com/valerytimofeev/composedstorage/categorydetail/CategoryDetailScreen.kt
+++ app/src/main/java/com/valerytimofeev/composedstorage/categorydetail/CategoryDetailScreen.kt
@@ -5,12 +5,13 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Icon
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AddCircle
 import androidx.compose.material.icons.filled.ArrowForward
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -20,6 +21,7 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.valerytimofeev.composedstorage.ui.theme.Mint
+import com.valerytimofeev.composedstorage.utils.DialogInputData
 
 
 @Composable
@@ -55,12 +57,15 @@ fun TopMenu(
         ) {
             Text(text = text, fontSize = 24.sp)
             Icon(
-                Icons.Filled.ArrowForward,
+                Icons.Filled.AddCircle,
                 contentDescription = "Icon",
                 modifier = Modifier
                     .padding(8.dp)
                     .fillMaxHeight()
                     .aspectRatio(1f)
+                    .clickable {
+
+                    }
             )
         }
     }
@@ -70,6 +75,9 @@ fun TopMenu(
 fun ItemsList(
     viewModel: CategoryDetailViewModel = hiltViewModel()
 ) {
+    if (viewModel.openChangeDialog.value) {
+        ChangeDialog()
+    }
     LazyColumn(contentPadding = PaddingValues(12.dp)) {
         items(count = viewModel.getStorageListSize()) {
             ItemEntry(
@@ -88,8 +96,10 @@ fun ItemEntry(
     size: List<String>,
     sizeType: List<String>,
     itemIndex: Int,
-    modifier: Modifier = Modifier
+    modifier: Modifier = Modifier,
+    viewModel: CategoryDetailViewModel = hiltViewModel()
 ) {
+
     Box(
         modifier = modifier
             .shadow(elevation = 4.dp, RoundedCornerShape(4.dp))
@@ -99,7 +109,8 @@ fun ItemEntry(
             .height(60.dp)
             .padding(4.dp)
             .clickable {
-                /*ToDo*/
+                viewModel.saveClickedStorage(name[itemIndex], size[itemIndex], sizeType[itemIndex])
+                viewModel.openChangeDialog.value = true
             }
     ) {
         Row(
@@ -127,4 +138,39 @@ fun ItemEntry(
         }
     }
     Spacer(modifier = Modifier.height(12.dp))
+}
+
+@Composable
+fun ChangeDialog(
+    viewModel: CategoryDetailViewModel = hiltViewModel()
+) {
+    AlertDialog(
+        onDismissRequest = { viewModel.openChangeDialog.value = false },
+        title = {
+            ItemEntry(
+                name = viewModel.loadClickedStorage().name,
+                size = viewModel.loadClickedStorage().size,
+                sizeType = viewModel.loadClickedStorage().sizeType,
+                itemIndex = 0
+            )
+        },
+        text = { Text(text = "Text") },
+        confirmButton = {
+            Button(
+                onClick = {
+                    viewModel.openChangeDialog.value = false
+                }) {
+                Text("This is the Confirm Button")
+            }
+        },
+        dismissButton = {
+            Button(
+                onClick = {
+                    viewModel.openChangeDialog.value = false
+                }) {
+                Text("This is the Confirm Button")
+            }
+        },
+
+        )
 }
--- app/src/main/java/com/valerytimofeev/composedstorage/categorydetail/CategoryDetailViewModel.kt
+++ app/src/main/java/com/valerytimofeev/composedstorage/categorydetail/CategoryDetailViewModel.kt
@@ -1,12 +1,18 @@
 package com.valerytimofeev.composedstorage.categorydetail
 
+import android.util.Log
 import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import com.valerytimofeev.composedstorage.data.database.StorageItem
+import com.valerytimofeev.composedstorage.utils.DialogInputData
+import com.valerytimofeev.composedstorage.utils.DialogOutputData
 
 class CategoryDetailViewModel: ViewModel() {
 
     val storageList = mutableStateListOf<StorageItem>()
+
+    val openChangeDialog = mutableStateOf(false)
 
     init {
         storageList += StorageItem(1, "Свинина", "Мясо", "кг.", 2.35f)
@@ -15,6 +21,7 @@ class CategoryDetailViewModel: ViewModel() {
         storageList += StorageItem(4, "Говядина", "Мясо", "кг.", 0.5f)
         storageList += StorageItem(5, "Фарш куриный", "Мясо", "кг.", 0.4f)
         storageList += StorageItem(6, "Фарш свиной", "Мясо", "кг.", 3.2f)
+        storageList += StorageItem(7, "Тестовое мясо", "Мясо", "кг.", 16.65f)
     }
 
     fun getStorageListSize(): Int {
@@ -31,6 +38,20 @@ class CategoryDetailViewModel: ViewModel() {
 
     fun getStorageSizeType(): List<String> {
         return storageList.map { it.sizeType }
+    }
+
+    val clickedStorage = mutableStateOf(DialogInputData("", "", ""))
+
+    fun saveClickedStorage(name: String, size: String, sizeType: String) {
+        clickedStorage.value = DialogInputData(name, size, sizeType)
+    }
+
+    fun loadClickedStorage(): DialogOutputData {
+        return DialogOutputData(
+            name = listOf(clickedStorage.value.name),
+            size = listOf(clickedStorage.value.size),
+            sizeType = listOf(clickedStorage.value.sizeType),
+        )
     }
 
 }
