be0e398e8e83142045d088d2c55cc583bfff55c0
llin first <90470991+Haartag@users.noreply.github.com>
2022-06-01 23:25:51 +0300

    Fix Category tile text

--- app/src/main/java/com/valerytimofeev/composedstorage/common/CommonComposeFunctions.kt
+++ app/src/main/java/com/valerytimofeev/composedstorage/common/CommonComposeFunctions.kt
@@ -24,6 +24,7 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
@@ -194,7 +195,13 @@ fun CategoryEntry(
                         .fillMaxWidth()
                 )
             }
-            Text(text = categoryName, style = textStyle)
+            Text(
+                text = categoryName,
+                style = textStyle,
+                textAlign = TextAlign.Center,
+                maxLines = 2,
+                overflow = TextOverflow.Ellipsis
+            )
         }
     }
 }
